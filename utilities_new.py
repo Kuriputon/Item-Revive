7e323a841dbaa5a0a6aa2727696d719e03b77e58
Kuriputon <118032853+Kuriputon@users.noreply.github.com>
2024-10-23 15:14:20 +0800

    Update utilities_new.py

--- utilities_new.py
+++ utilities_new.py
@@ -6,5 +6,5 @@ def resource_path(relative_path):
         # 如果在打包后的程序中运行
         return os.path.join(sys._MEIPASS, relative_path)
     # 在开发环境中运行
-    return os.path.join(os.path.abspath("."), relative_path)
+    return relative_path
 
